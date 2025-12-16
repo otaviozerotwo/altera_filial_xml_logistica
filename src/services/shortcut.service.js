99caa021e8def46bb3a6d59c26b3b253689e895f
Otavio <otavio.zerotwo@gmail.com>
2025-12-15 21:00:42 -0300

    fix: add URL_TELEVENDAS no .env

--- src/services/shortcut.service.js
+++ src/services/shortcut.service.js
@@ -3,9 +3,10 @@ const javaService = require('./java.service');
 
 exports.executarPrograma = () => {
   const javaws = javaService.getJavaWsPath();
+  const URL_TELEVENDAS = process.env.URL_TELEVENDAS;
 
   spawn(javaws, [
-    'http://192.0.1.11:8081/vendas-server/app/logistica-client.jnlp'
+    `${URL_TELEVENDAS}/app/logistica-client.jnlp`
   ], {
     detached: true,
     stdio: 'ignore'
